dbdf576551e4ea7893cb08be1a131dc0f957d94f
Stepanov <daniil.stepanov@requestum.com>
2018-11-02 12:24:22 +0200

    carousels - version one, just functionality and basic layouts

--- landing-page/src/js/main.js
+++ landing-page/src/js/main.js
@@ -2,4 +2,29 @@
 
 import '@babel/polyfill';
 import 'dom4';
-// import $ from 'jquery';
+import $ from 'jquery';
+
+$(document).ready(function () {
+  $('.main-carousel').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1
+    // adaptiveHeight: true
+  });
+
+  $('.about-carousel').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1
+  });
+
+  $('.reviews-carousel').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    centerMode: true,
+    centerPadding: '0',
+    slidesToShow: 3
+  });
+});
